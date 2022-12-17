5b9f24b70e179eb0243e37a4d4ded53123d12def
RomanZorkin <zorkin.roman@gmail.com>
2022-12-17 16:48:02 +0700

    feature: implement extensions rules in source.py

--- service/worker/source.py
+++ service/worker/source.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 import httpx
+from fastapi import HTTPException
 
 from service import schemas
 from service.repos.rule import EndpointRepo
@@ -9,30 +10,38 @@ endpoint_repo = EndpointRepo()
 
 
 def get_settings(uid: int) -> schemas.Endpoint:
-
     rules = endpoint_repo.get_by_uid(uid=uid)
     endpoint_config = schemas.Endpoint.from_orm(rules)
-
     endpoint_config.data.clubid = endpoint_config.clubid
-    
     return endpoint_config
 
 
 def get_response(endpoint_config: schemas.Endpoint):
     try:
-        return httpx.post(
+        response = httpx.post(
             endpoint_config.url,
             auth=(endpoint_config.user, endpoint_config.password),
             json=endpoint_config.data.dict(),
         )
-    except httpx.RequestError as exc:
-        return exc.request.content
+        if response.status_code != 200:
+            text = response.text            
+            raise HTTPException(status_code=500, detail=f'Server error {text}')
+        return response
+
+    except httpx.HTTPError as exc:
+        raise HTTPException(status_code=500, detail=f'Server error {exc}')
 
 
 def get_team() -> list[dict[str, Any]]:
-    endpoint_config = get_settings(uid=2)        
+    endpoint_config = get_settings(uid=2)
     response = get_response(endpoint_config)
+    team = response.json().get(endpoint_config.contentname, None)
 
-    team = response.json()[endpoint_config.contentname]
+    if not team:
+        raise HTTPException(
+            status_code=400,
+            detail='Error processing the received data. Perhaps the structure of the response has\
+        changed',
+        )
 
     return [schemas.Staff(**person).dict(by_alias=False) for person in team]
